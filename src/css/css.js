62fc98ad96b3c2ff7e432d71b4b791fde50405d7
Atul Kumar <akumarofficial4224@gmail.com>
2021-09-21 10:10:32 +0530

    fix an error

--- src/css/css.js
+++ src/css/css.js
@@ -1,9 +1,9 @@
 // Function: Sets the inline css property of an this.element
 function css(keyValueObj=null){
+    const keys = Object.keys(keyValueObj);
     if(keyValueObj){
-        const keys = Object.keys(keyValueObj);
         for(let i = 0; i < keys.length; i++){
-            this.this.element.style[keys[i]] = keyValueObj[keys[i]];
+            this.element.style[keys[i]] = keyValueObj[keys[i]];
         }
     }
     else{
