f3eeb70d21d5bad5267a2456fa51333ad56b136b
Atul Kumar <akumarofficial4224@gmail.com>
2021-09-23 18:25:21 +0530

    Changed the entry point, create index.js and delete index.html and jquery.html

---
+++ index.js
@@ -0,0 +1,17 @@
+import { get } from './ajax/get.js';
+import { post } from './ajax/post.js';
+import { hide, show, blink, move, grow, shrink, shrinkAndGrow, rotate } from './animation/animation.js';
+import { attr, hasAttr, removeAttr } from './attribute/attribute.js';
+import { hasClass, hasClasses, removeClass, removeClasses, addClass, addClasses, classes } from './class/class.js';
+import { log, cclear, debug, cerror, cgrp, cinfo, cmem, ctbl, cwarn, ctime, ctimend, ctrc } from './console/console.js';
+import { css } from './css/css.js';
+import { data } from './data/data.js';
+import { Stopwatch } from './delay_interval/stopwatch.js';
+import { countChildren, html, text, clone, insert, empty, remove, parent, parents, siblings, prevSiblings, nextSiblings, position, scale, before, after, instantiate } from './dom/dom.js';
+import { error } from './error/error.js';
+import { on, off } from './event/events.js';
+import { getData } from './fetch/fetch.js';
+import { hasId, removeId, changeId, id } from './id/id.js';
+import { isInt, isBool, isStr, isDecimal, isObj, isFunc, isSym, isUndefined, isNull, isArray } from './is_functions/is.js';
+import { $ } from './selector/selector.js';
+import { Video } from './video/video.js';
